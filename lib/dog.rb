b15ba02f18c8c8733e1b5ebd96481d3bf4c81059
hugo valois <hvgaming@yahoo.com>
2018-11-08 01:06:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
   
-  attr_accessor :id, :name, :grade
+  attr_accessor :id, :name, :breed
   
-  def initialize(id=nil, name, )
-   	  @id, @name, @grade = id, name, grade
+  def initialize(id=nil,name,breed)
+   	  @id, @name, @breed = id, name, breed;
   end
   
   
