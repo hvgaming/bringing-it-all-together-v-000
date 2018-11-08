5bf587081f6ecd9e286112bf12e74ac09ee941f8
hugo valois <hvgaming@yahoo.com>
2018-11-08 01:09:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,7 +16,19 @@ class Dog
    def save
      
    end
+   def self.create(name, breed)
    
+  end
+   def self.new_from_db(row)
+    
+  end 
+   def self.find_by_name(name)
+  
+  end
+   def update
+    
+  end
+ 
   
   
   
