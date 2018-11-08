7cfce02d81cdc6f03a519b431f542914bab82aba
hugo valois <hvgaming@yahoo.com>
2018-11-08 01:10:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,6 @@
 class Dog 
   
-  attr_accessor :id, :name, :breed
   
-  def initialize(id=nil,name,breed)
-   	  @id, @name, @breed = id, name, breed
-  end
-  
-   def self.create_table 
-     
-   end
-   
-   def self.drop_table 
-     
-   end
-   def save
-     
-   end
-   def self.create(name, breed)
-   
-  end
-   def self.new_from_db(row)
-    
-  end 
-   def self.find_by_name(name)
-  
-  end
-   def update
     
   end
  
